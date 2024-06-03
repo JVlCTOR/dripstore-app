248e84a0f2ae72f920b2a3a396bb46a0e1dc030c
JVlCTOR <joaovictorresende59@gmail.com>
2024-06-03 19:53:20 -0300

    configurando api axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import DataTable from "./components/table/DataTable";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const getProdutos = async () => {
+    const URL_PRODUTOS = "https://dripstore-api.onrender.com/api/produto";
+    try {
+      const res = await axios.get(URL_PRODUTOS);
+      console.log(res);
+    } catch (error) {
+      console.log(`erro ao buscar o produto ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getProdutos();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <DataTable />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
